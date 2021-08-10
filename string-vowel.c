47cc0cb1478595c02b08b9ba4e5b60379ccad17f
neocollege <86466839+neocollege@users.noreply.github.com>
2021-08-10 20:30:28 +0530

    Update string-vowel.c

--- string-vowel.c
+++ string-vowel.c
@@ -1,11 +1,11 @@
-//WAP to accept a string and count the number of vowels and consonants and numbers
+//WAP to accept a string and count the number of vowels and consonants and numbers and blank spaces
 
 #include <stdio.h>
 #include <string.h>
 
 void main() {
     char s[100];
-    int c=0,i,v=0,num=0,cons=0;
+    int c=0,i,v=0,num=0,cons=0,bs=0;
     printf("Enter a string: \n");
     gets(s);
     for(i=0;s[i]!='\0';i++)
@@ -15,10 +15,13 @@ void main() {
             v++;
         else if(s[i]=='0'||s[i]=='1'||s[i]=='2'||s[i]=='3'||s[i]=='4'||s[i]=='5'||s[i]=='6'||s[i]=='7'||s[i]=='8'||s[i]=='9')
             num++;
+        else if(s[i]==' ')
+            bs++;
         else
             cons++;
     }
     printf("The number of numbers is: %d\n",num);
     printf("The number of vowels is: %d\n",v);
     printf("The number of consonants is: %d\n",cons);
+    printf("The number of blank spaces is: %d\n",bs);
 }
