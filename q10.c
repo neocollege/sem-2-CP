dcf3c6ba346cafe32b7a087a9c0e5f40ee9365aa
neocollege <86466839+neocollege@users.noreply.github.com>
2021-08-11 18:56:34 +0530

    Create q10.c

---
+++ q10.c
@@ -0,0 +1,20 @@
+  
+// WAP to display first n elements of Fibonacci series (using 'for') 
+#include <stdio.h>
+
+int main()
+{
+    int n,i,a=1,b=1,c;
+    printf("Enter a number : \n");
+    scanf("%d",&n);
+    printf("1 1 ");
+    for(i=1;i<=n-2;i++)
+    {
+        c = a+b;
+        printf("%d ",c);
+        a=b;
+        b=c;
+    }
+    
+    return 0;
+}
