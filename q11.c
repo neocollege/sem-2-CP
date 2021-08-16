45ec9080123587750b8272b361faa3566d8bd9c9
neocollege <86466839+neocollege@users.noreply.github.com>
2021-08-16 17:54:15 +0530

    Update q11.c

--- q11.c
+++ q11.c
@@ -1,20 +1,19 @@
 // WAP to find nCr using function
 #include <stdio.h>
 
-int main()
+int factorial(int x)
+{
+    int fact=1;
+    for(int i=1;i<=x;i++)
+        fact = fact*i;
+    return fact;
+}
+    
+void main()
 {
     int n,r,c;
     printf("Enter n and r \n");
     scanf("%d %d",&n,&r);
-    int factorial(int x)
-    {
-        int fact=1;
-        for(int i=1;i<=x;i++)
-            fact = fact*i;
-        return fact;
-    }
     c = factorial(n)/(factorial(n-r)*factorial(r));
     printf("nCr = %d",c);
-    
-    return 0;
 }
